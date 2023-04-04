893557fcf4f53884e293391027f1ad22033fc622
Leon <leon@leons-air.home>
2023-04-04 23:36:53 +0100

    server second

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_DB_URL)
 
+if (PORT){
+    app.listen(PORT,()=>{
+        console.log(`listening on port ${PORT}`)
+    })
+}
 
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`)
-})
+export {app} 
